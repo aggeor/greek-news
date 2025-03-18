7e8102b9a714d6d9853eb4c310ac0c6d27362c17
Aggelos Georgiadis <ag.gewr@gmail.com>
2025-03-18 11:54:55 +0200

    fix card layout

--- src/app/components/ArticleCard.tsx
+++ src/app/components/ArticleCard.tsx
@@ -1,4 +1,3 @@
-import { AspectRatio } from "@/components/ui/aspect-ratio"
 import {
   Card,
   CardContent,
@@ -15,22 +14,15 @@ interface ArticleCardProps{
 
 export function ArticleCard({ article }: ArticleCardProps) {
   return (
-    <Card className="w-[40vw] h-[20vh] mt-3">
-      <CardHeader>
-        <div className="flex flex-row">
-            <Image src={article.thumbnail} width={64} height={50} alt="Image" className="rounded-md object-cover" />
-            <div className="flex flex-col">
-                <CardTitle>{article.title}</CardTitle>
-            </div>
-        </div>
+    <Card className="grid grid-cols-[auto_1fr] max-w-3xl p-2 gap-1">
+      {article.thumbnail ? <Image src={article.thumbnail} width={256} height={256} alt="Image" className="rounded-md object-cover" /> : <div/>}
+      <CardHeader className="overflow-hidden">
+            <CardTitle className="text-lg line-clamp-2">{article.title}</CardTitle>
+            <div>{article.author}</div>
+            <div>{article.pubDate}</div>
+            <CardDescription>{article.description}</CardDescription>
+            <div>Διαβαστε Περισσότερα</div>
       </CardHeader>
-      <CardContent>
-        {/* <CardDescription>{article.description}</CardDescription> */}
-        {article.pubDate}
-        </CardContent>
-      <CardFooter>
-        Περισσότερα
-      </CardFooter>
     </Card>
   )
 }
